96a42b41ccb2597ab39ccf1ab1d794ff425aeaf8
Simon Leyder <sim.leyder@gmail.com>
2014-10-20 16:23:33 +0200

    Init Project with Grunt

---
+++ README.md
@@ -0,0 +1,12 @@
+#restoRallye
+
+##Projet web 2014-2015
+
+***
+
+Réalisation d’un site web en groupe à l’aide d’un framework **PHP** dans le cadre du cours de Projet Web.
+
+**Groupe :** [Luc Matagne](http://luc-matagne.be/Portfolio/) & [Simon Leyder](http://portfolio.simon-leyder.be/)
+
+***
+
---
+++ mockUp/Gruntfile.js
@@ -0,0 +1,62 @@
+"user strict";
+
+module.exports = function( grunt ) {
+
+	grunt.loadNpmTasks( "grunt-contrib-sass" );
+	grunt.loadNpmTasks( "grunt-contrib-jade" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+
+	grunt.initConfig( {
+		"sass": {
+			"styles": {
+		      "options": {
+		        "style": 'expanded'
+		      },
+		      "files": {
+		        "lib/css/screen.css": "src/sass/screen.sass"
+		      }
+		    }
+		},
+		"jade": {
+			"jades": {
+				"option": {
+					"pretty": true,
+					"data": {
+						"debug": false,
+					},
+				},
+				"files": {
+					"lib/index.html": "src/jade/index.jade"
+				}
+			}
+		},
+		"uglify": {
+			"scripts": {
+				"files": {
+					"lib/js/script.min.js": "src/js/script.js"
+				}
+			}
+		},
+		"watch": {
+			"styles": {
+				"files": [ "src/sass/*.sass" ],
+				"tasks": [ "sass:styles" ]
+			},
+			"jades": {
+				"files": [ "src/jade/*.jade" ],
+				"tasks": [ "jade:jades" ]
+			},
+			"scripts": {
+				"files": [ "src/js/script.js" ],
+				"tasks": [ "uglify:scripts" ]
+			}
+		}
+	} );
+
+	grunt.registerTask( "build", [
+		"sass",
+		"uglify"
+	] );
+};
