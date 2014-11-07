ddb064e7ba34fd8d7eb79dfacce8a4eb096dd714
Quelu <quelumatagne@gmail.com>
2014-11-07 19:12:05 +0100

    Restaurant list

--- dev/app/views/restaurants/adminIndex.blade.php
+++ dev/app/views/restaurants/adminIndex.blade.php
@@ -1,9 +1,42 @@
 @extends('layouts.admin')
 
 @section('content')
-    <ul>
-        @foreach($restaurants as $restaurant)
-            <li>{{ $restaurant->name }}</li>
-        @endforeach
-    </ul>
+    <h2 class="sub-header">Liste des restaurants</h2>
+
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Adresse</th>
+                    <th>Site</th>
+                    <th>Email</th>
+                    <th>Téléphonne</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($restaurants as $restaurant)
+                    <tr>
+                        <td>{{ $restaurant->name }}</td>
+                        <td>{{ $restaurant->adress . ' ' . $restaurant->adress_number }}<br>
+                            {{ $restaurant->postal_code . ' ' . $restaurant->city }}</td>
+                        <td>{{ $restaurant->website }}</td>
+                        <td>{{ $restaurant->email }}</td>
+                        <td>{{ $restaurant->tel }}</td>
+                        <td>
+                            <a href="{{ route('admin.restaurants.edit') }}">
+                                <button class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                </button>
+                            </a>
+                            <button class="btn btn-danger">
+                                <span class="glyphicon glyphicon-remove"></span>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @stop
