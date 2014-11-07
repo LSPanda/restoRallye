5b82fc8913142c8aa3c4803ef25ddc25971dfc41
Quelu <quelumatagne@gmail.com>
2014-11-07 16:45:31 +0100

    Local env variable

---
+++ dev/.env.local.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'DB_HOST'     => 'localhost',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root',
+    'DB_NAME'     => 'restoRallye',
+    'PREFIX'      => 'admin'
+];
