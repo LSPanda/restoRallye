bb39691f7aa8c6a750bd81f79c1f52b4c249c5b3
Quelu <quelumatagne@gmail.com>
2014-11-09 16:23:32 +0100

    Active status sidebar

--- dev/app/views/partials/nav/adminSideBar.blade.php
+++ dev/app/views/partials/nav/adminSideBar.blade.php
@@ -1,20 +1,15 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li><a href="{{ route('adminIndex') }}">Tableau de bord</a></li>
+        <li{{ Request::is( 'admin') ? ' class="active"' : '' }}><a href="{{ route('adminIndex') }}">Tableau de bord</a></li>
     </ul>
     <hr>
     <ul class="nav nav-sidebar">
-        <li><a href="{{ route('restaurantAdminIndex') }}">Restaurants</a></li>
-        <li><a href="{{ route('admin.menus.index') }}">Menus</a></li>
-        <li><a href="{{ route('rallyeAdminIndex') }}">Rallyes</a></li>
-        <li><a href="{{ route('postAdminIndex') }}">Articles</a></li>
-        <li><a href="{{ route('pageAdminList') }}">Contenus</a></li>
-        <li><a href="{{ route('userAdminIndex') }}">Utilisateurs</a></li>
-    </ul>
-    <ul class="nav nav-sidebar">
-        <li><a href=""></a></li>
+        <li{{ Request::is( 'admin/restaurants*') ? ' class="active"' : '' }}><a href="{{ route('restaurantAdminIndex') }}">Restaurants</a></li>
+        <li{{ Request::is( 'admin/menus*') ? ' class="active"' : '' }}><a href="{{ route('admin.menus.index') }}">Menus</a></li>
+        <li{{ Request::is( 'admin/rallye*') ? ' class="active"' : '' }}><a href="{{ route('rallyeAdminIndex') }}">Rallyes</a></li>
+        <li{{ Request::is( 'admin/posts*') ? ' class="active"' : '' }}><a href="{{ route('postAdminIndex') }}">Articles</a></li>
+        <li{{ Request::is( 'admin/contents*') ? ' class="active"' : '' }}><a href="{{ route('pageAdminList') }}">Contenus</a></li>
+        <li{{ Request::is( 'admin/users*') ? ' class="active"' : '' }}><a href="{{ route('userAdminIndex') }}">Utilisateurs</a></li>
     </ul>
     <hr>
-    {{ Request::path() }}
 </div>
-
