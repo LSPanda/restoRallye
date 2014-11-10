16b6dfb9b850f77051d0407fed9e89a3b4630a61
Quelu <quelumatagne@gmail.com>
2014-11-10 13:22:48 +0100

    Disposition des champs

--- dev/app/views/restaurants/admin/edit.blade.php
+++ dev/app/views/restaurants/admin/edit.blade.php
@@ -20,59 +20,55 @@
         <div class="row">
             <div class="col-md-5">
                 <div class="form-group">
+                    {{ Form::label('email', 'Email') }}
+                    <div class="input-group">
+                        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+                        {{ Form::email('email', $restaurant->email, ['class' => 'form-control', 'placeholder' => 'email@domaine.com']) }}
+                        {{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('website', 'Site internet') }}
+                    <div class="input-group">
+                        <span class="input-group-addon glyphicon glyphicon-globe"></span>
+                        {{ Form::url('website', $restaurant->website, ['class' => 'form-control', 'placeholder' => 'http://nom-du-site.com']) }}
+                        {{ $errors->first('website', '<div class="alert alert-danger">:message</div>') }}
+                    </div>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('tel', 'Site internet') }}
+                    <div class="input-group">
+                        <span class="input-group-addon glyphicon glyphicon-earphone"></span>
+                        {{ Form::text('tel', $restaurant->tel, ['class' => 'form-control', 'placeholder' => '0*********']) }}
+                        {{ $errors->first('tel', '<div class="alert alert-danger">:message</div>') }}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="form-group">
                     {{ Form::label('adress', 'Adresse du restaurant') }}
                     {{ Form::text('adress', $restaurant->adress, ['class' => 'form-control', 'require']) }}
                     {{ $errors->first('adress', '<div class="alert alert-danger">:message</div>') }}
                 </div>
+                <div class="row form-group">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            {{ Form::label('postal_code', 'Code postal') }}
+                            {{ Form::text('postal_code', $restaurant->postal_code, ['class' => 'form-control', 'require', 'maxlength' => 4]) }}
+                            {{ $errors->first('postal_code', '<div class="alert alert-danger">:message</div>') }}
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            {{ Form::label('city', 'Ville') }}
+                            {{ Form::text('city', $restaurant->city, ['class' => 'form-control', 'require']) }}
+                            {{ $errors->first('city', '<div class="alert alert-danger">:message</div>') }}
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-lg btn-primary pull-right">Éditer</button>
             </div>
         </div>
-        <div class="row form-group">
-            <div class="col-md-2">
-                <div class="form-group">
-                    {{ Form::label('postal_code', 'Code postal') }}
-                    {{ Form::text('postal_code', $restaurant->postal_code, ['class' => 'form-control', 'require', 'maxlenght' => 4]) }}
-                    {{ $errors->first('postal_code', '<div class="alert alert-danger">:message</div>') }}
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="form-group">
-                    {{ Form::label('city', 'Ville') }}
-                    {{ Form::text('city', $restaurant->city, ['class' => 'form-control', 'require']) }}
-                    {{ $errors->first('city', '<div class="alert alert-danger">:message</div>') }}
-                </div>
-            </div>
-        </div>
-        <div class="row form-group">
-            <div class="col-md-5">
-                {{ Form::label('email', 'Email') }}
-                <div class="input-group">
-                    <span class="input-group-addon glyphicon glyphicon-envelope"></span>
-                    {{ Form::email('email', $restaurant->email, ['class' => 'form-control', 'placeholder' => 'email@domaine.com']) }}
-                    {{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
-                </div>
-            </div>
-        </div>
-        <div class="row form-group">
-            <div class="col-md-5">
-                {{ Form::label('website', 'Site internet') }}
-                <div class="input-group">
-                    <span class="input-group-addon glyphicon glyphicon-globe"></span>
-                    {{ Form::url('website', $restaurant->website, ['class' => 'form-control', 'placeholder' => 'http://nom-du-site.com']) }}
-                    {{ $errors->first('website', '<div class="alert alert-danger">:message</div>') }}
-                </div>
-            </div>
-        </div>
-        <div class="row form-group">
-            <div class="col-md-5">
-                {{ Form::label('tel', 'Site internet') }}
-                <div class="input-group">
-                    <span class="input-group-addon glyphicon glyphicon-earphone"></span>
-                    {{ Form::text('tel', $restaurant->tel, ['class' => 'form-control', 'placeholder' => '0*********']) }}
-                    {{ $errors->first('tel', '<div class="alert alert-danger">:message</div>') }}
-                </div>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-default">Éditer</button>
     {{ Form::close() }}
 @stop
 
