a68585232aed1cd862814005c4f1fa417740a5bf
Simon Leyder <sim.leyder@gmail.com>
2014-11-11 20:17:19 +0100

    googleMap fixed

--- mockUp/src/js/script.js
+++ mockUp/src/js/script.js
@@ -8,11 +8,24 @@
 ( function( $ ) {
     "user strict";
 
+    var generateGoogleMap = function() {
+        gMap = new google.maps.Map( document.getElementById("slideMap"), {
+            center: new google.maps.LatLng( 50.846686, 4.352425 ),
+            zoom: 12,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAPx
+        } );
+    }
+
     $( function() {
         //Detect scroll for stick and reduce my nav-menu
         $( "body" ).scroll( function() {
             $( "header nav" ).addClass( "stick" );
         } );
+
+        generateGoogleMap();
     } );
 
 } ) (jQuery);
