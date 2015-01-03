0eac22566a5cd83a93b5855063f67719ab093e06
Simon <Sim.Leyder@gmail.com>
2015-01-03 18:56:58 +0100

    Change mockup to personnal Workflow

---
+++ _mockUp/Gruntfile.js
@@ -0,0 +1,81 @@
+"user strict";
+
+module.exports = function( grunt ) {
+
+	grunt.loadNpmTasks( "kouto-swiss" );
+	grunt.loadNpmTasks( "grunt-contrib-stylus" );
+	grunt.loadNpmTasks( "grunt-contrib-jade" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-notify" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+
+	grunt.initConfig( {
+		"stylus": {
+			"styles": {
+		      "options": {
+		        "compress": false, // - Change value to have lisible CSS
+				"use": [
+					require("kouto-swiss")
+				]
+		      },
+		      "files": {
+		        "bin/css/main.css": "src/styl/main.styl"
+		      }
+		    }
+		},
+		"jade": {
+			"html": {
+				"options": {
+					"pretty": false, // - Change value to have lisible HTML
+					"data": {
+						"debug": false,
+					},
+				},
+				"files": { // - Add jade's fill to compile here
+					"bin/html/blog.html": "src/jade/blog.jade",
+					"bin/html/contact.html": "src/jade/contact.jade",
+					"bin/html/evenements.html": "src/jade/evenements.jade",
+					"bin/html/index.html": "src/jade/main.jade",
+					"bin/html/restaurants.html": "src/jade/restaurants.jade"
+				}
+			}
+		},
+		"uglify": {
+			"scripts": {
+				"files": {
+					"bin/js/script.js": "src/js/script.js"
+				}
+			}
+		},
+		"notify_hooks": {
+			"options": {
+				"enable": true,
+				"success": true, // - Change value to see notify only for errors
+				"duration": 1
+			}
+		},
+		"watch": {
+			"styles": {
+				"files": [ "src/styl/*.styl" ],
+				"tasks": [ "stylus:styles", "notify_hooks" ]
+			},
+			"html": {
+				"files": [ "src/jade/**/*.jade" ],
+				"tasks": [ "jade:html", "notify_hooks" ]
+			},
+			"scripts": {
+				"files": [ "src/js/script.js" ],
+				"tasks": [ "uglify:scripts", "notify_hooks" ]
+			}
+		}
+	} );
+
+	grunt.registerTask( "work", [
+		"jade",
+		"stylus",
+		"uglify",
+		"notify_hooks",
+		"watch"
+	] );
+};
---
+++ _mockUp/src/js/script.js
@@ -0,0 +1,67 @@
+/* Resto-Rally
+*
+* Script for my website
+*
+* Started @ 04/11/2014
+*/
+
+( function( $ ) {
+    "user strict";
+
+    var defaultPosition,
+    mapOptions,
+    gMap,
+    Geocoder,
+    gMarker,
+    image;
+
+    function generateGoogleMap() {
+        //Set position to Bruxelle
+        defaultPosition = new google.maps.LatLng( 50.8504500, 4.3487800 );
+        //Init mapOptions
+        mapOptions = {
+            center: defaultPosition,
+            zoom: 17,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAPx
+        };
+        //Marker personnalisé
+        image = {
+            url: '../css/images/marker.png',
+            size: new google.maps.Size( 55,60 ),
+            origin: new google.maps.Point( 0,0 ),
+            anchor: new google.maps.Point( 20, 60 )
+        };
+        //Init Geocoder
+        Geocoder = new google.maps.Geocoder();
+        selectedAdress();
+        //Set defaultGmap
+        gMap = new google.maps.Map( document.getElementById( "slideMap" ), mapOptions );
+    }
+
+    function selectedAdress() {
+        Geocoder.geocode( { 'address': myAddress}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                gMap.setCenter(results[0].geometry.location);
+                gMarker = new google.maps.Marker( {
+                    map: gMap,
+                    position: results[0].geometry.location,
+                    icon: image
+                } );
+            }
+        } );
+    }
+
+    $( function() {
+        //Detect scroll for stick and reduce my nav-menu
+        $( "body" ).scroll( function() {
+            $( "header nav" ).addClass( "stick" );
+        } );
+
+        //Generate google gMap for event
+        generateGoogleMap();
+    } );
+
+} ) (jQuery);
