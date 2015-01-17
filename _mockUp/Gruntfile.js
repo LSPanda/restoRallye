5f2ed9fbc37f107c5acfbfd4522b9169c518feac
Simon <Sim.Leyder@gmail.com>
2015-01-17 15:38:08 +0100

    Responsive : footer

--- _mockUp/Gruntfile.js
+++ _mockUp/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function( grunt ) {
 		"jade": {
 			"html": {
 				"options": {
-					"pretty": true, // - Change value to have lisible HTML
+					"pretty": false, // - Change value to have lisible HTML
 					"data": {
 						"debug": false,
 					},
