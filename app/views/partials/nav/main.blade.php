0f9235b3f645442cc975f66b909fbad3d8a4b906
Quelu <quelumatagne@gmail.com>
2015-01-17 23:27:16 +0100

    Integration : Responsive

--- app/views/partials/nav/main.blade.php
+++ app/views/partials/nav/main.blade.php
@@ -1,24 +1,23 @@
 <header itemscope itemtype="http://schema.org/WPHeader" class="header">
-    <h1 itemprop="headline" class="hiddenTitle">Bienvenue sur le site de Resto Rallye</h1>
+<h1 itemprop="headline" class="hiddenTitle">Bienvenue sur le site de Resto Rallye </h1>
     <nav itemscope itemtype="http://schema.org/SiteNavigationElement" class="header__nav">
         <div class="nav">
             <h2 itemprop="alternativeHeadline" class="hiddenTitle">Menu de navigation du site</h2>
-            <span itemprop="keywords" class="inline-block nav__element">
-                <a itemprop="url" href="{{ route('rallyes.index') }}" class="element__link">Événements</a>
-            </span>
-            <span itemprop="keywords" class="inline-block nav__element">
-                <a itemprop="url" href="{{ route('posts.index') }}" class="element__link">Actualités</a>
-            </span>
-            <span class="inline-block banners">
-                <a itemprop="url" href="{{ route('home') }}" class="banners__link">
-                <img itemprop="image" src="/css/images/links/Ecusson.png" alt="Banners Resto Rallye"></a>
-            </span>
-            <span itemprop="keywords" class="inline-block nav__element">
-                <a itemprop="url" href="{{ route('restaurants.index') }}" class="element__link">Restaurants</a>
-            </span>
-            <span itemprop="keywords" class="inline-block nav__element">
-                <a itemprop="url" href="{{ route('contact') }}" class="element__link">Contact</a>
-            </span>
+                <span itemprop="keywords" class="inline-block nav__element">
+                    <a itemprop="url" href="{{ route('rallyes.index') }}" class="element__link">Événements</a>
+                </span>
+                <span itemprop="keywords" class="inline-block nav__element">
+                    <a itemprop="url" href="{{ route('posts.index') }}" class="element__link">Actualités</a>
+                </span>
+                <span class="inline-block banners">
+                    <a itemprop="url" href="{{ route('home') }}" class="banners__link"><img itemprop="image" src="/css/images/links/Ecusson.png" alt="Banners Resto Rallye"></a>
+                </span>
+                <span itemprop="keywords" class="inline-block nav__element">
+                    <a itemprop="url" href="{{ route('restaurants.index') }}" class="element__link">Restaurants</a>
+                </span>
+                <span itemprop="keywords" class="inline-block nav__element">
+                    <a itemprop="url" href="{{ route('contact') }}" class="element__link">Contact</a>
+                </span>
         </div>
     </nav>
 </header>
