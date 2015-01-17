edc770f239d536f3361d125b80304894ecdb43c5
Quelu <quelumatagne@gmail.com>
2015-01-18 00:04:45 +0100

    Environement file

---
+++ .env.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DB_HOST'     => getenv('DB_HOST'),
+    'DB_USERNAME' => getenv('DB_USERNAME'),
+    'DB_PASSWORD' => getenv('DB_PASSWORD'),
+    'DB_NAME'     => getenv('DB_NAME'),
+    'DB_PREFIX'   => getenv('DB_PREFIX'),
+    'PREFIX'      => getenv('PREFIX')
+    ];
