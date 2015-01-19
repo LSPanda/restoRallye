2000e35222022796c2f01406c3df555b7bdaf85f
Quelu <quelumatagne@gmail.com>
2015-01-19 21:37:06 +0100

    Admin menu

--- app/views/partials/nav/admin/main.blade.php
+++ app/views/partials/nav/admin/main.blade.php
@@ -7,13 +7,20 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">RestoRallye</a>
+            <a class="navbar-brand" href="{{ route('admin.home') }}">RestoRallye</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{ route('admin.home') }}">Dashboard</a></li>
                 <li><a href="{{ route('home') }}">Voir le site</a></li>
                 <li><a href="#">Profil</a></li>
+                <li class="text-center"><a>Gestion</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.restaurants.index') }}">Restaurants</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.menus.index') }}">Menus</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.rallyes.index') }}">Rallyes</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.posts.index') }}">Articles</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.pages.index') }}">Contenus</a></li>
+                <li class="visible-xs-block"><a href="{{ route('admin.users.index') }}">Utilisateurs</a></li>
                 <li><a href="{{ route('logout') }}">Déconnexion</a></li>
             </ul>
             <form class="navbar-form navbar-right">
