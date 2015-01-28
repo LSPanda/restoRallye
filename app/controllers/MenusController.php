b842420e12b11a3d64221b828cb81ca64ef21f84
Quelu <quelumatagne@gmail.com>
2015-01-28 18:26:13 +0100

    List menus in admin

--- app/controllers/MenusController.php
+++ app/controllers/MenusController.php
@@ -2,106 +2,131 @@
 
 class MenusController extends \BaseController {
 
-	/**
-	 * Display a listing of menus
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$menus = Menu::all();
+    /**
+     * Display a listing of menus
+     *
+     * @return Response
+     */
+    public function index () {
+        $menus = $this->getAllMenus ();
 
-		return View::make('menus.admin.index', compact('menus'));
-	}
+        {
+            return View::make ( 'menus.admin.index', compact ( 'menus' ) );
+        }
+    }
 
-	/**
-	 * Show the form for creating a new menus
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return View::make('menus.create');
-	}
+    /**
+     * Show the form for creating a new menus
+     *
+     * @return Response
+     */
+    public function create () {
+        return View::make ( 'menus.create' );
+    }
 
-	/**
-	 * Store a newly created menus in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$validator = Validator::make($data = Input::all(), Menu::$rules);
+    /**
+     * Store a newly created menus in storage.
+     *
+     * @return Response
+     */
+    public function store () {
+        $validator = Validator::make ( $data = Input::all (), Menu::$rules );
 
-		if ($validator->fails())
-		{
-			return Redirect::back()->withErrors($validator)->withInput();
-		}
+        if ($validator->fails ())
+        {
+            return Redirect::back ()->withErrors ( $validator )->withInput ();
+        }
 
-		Menu::create($data);
+        Menu::create ( $data );
 
-		return Redirect::route('menus.index');
-	}
+        return Redirect::route ( 'menus.index' );
+    }
 
-	/**
-	 * Display the specified menus.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$menus = Menu::findOrFail($id);
+    /**
+     * Display the specified menus.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function show ($id) {
+        $menus = Menu::findOrFail ( $id );
 
-		return View::make('menus.show', compact('menus'));
-	}
+        return View::make ( 'menus.show', compact ( 'menus' ) );
+    }
 
-	/**
-	 * Show the form for editing the specified menus.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$menus = Menu::find($id);
+    /**
+     * Show the form for editing the specified menus.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function edit ($id) {
+        $menus = Menu::find ( $id );
 
-		return View::make('menus.edit', compact('menus'));
-	}
+        return View::make ( 'menus.edit', compact ( 'menus' ) );
+    }
 
-	/**
-	 * Update the specified menus in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		$menus = Menu::findOrFail($id);
+    /**
+     * Update the specified menus in storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function update ($id) {
+        $menus = Menu::findOrFail ( $id );
 
-		$validator = Validator::make($data = Input::all(), Menu::$rules);
+        $validator = Validator::make ( $data = Input::all (), Menu::$rules );
 
-		if ($validator->fails())
-		{
-			return Redirect::back()->withErrors($validator)->withInput();
-		}
+        if ($validator->fails ())
+        {
+            return Redirect::back ()->withErrors ( $validator )->withInput ();
+        }
 
-		$menus->update($data);
+        $menus->update ( $data );
 
-		return Redirect::route('menus.index');
-	}
+        return Redirect::route ( 'menus.index' );
+    }
 
-	/**
-	 * Remove the specified menus from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		Menu::destroy($id);
+    /**
+     * Remove the specified menus from storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function destroy ($id) {
+        Menu::destroy ( $id );
 
-		return Redirect::route('menus.index');
-	}
+        return Redirect::route ( 'menus.index' );
+    }
+
+    /**
+     * Get the menu in json for the id given
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function getMenu ($id) {
+        $menu = Menu::find ( $id );
+        $menu = explode ( '[', $menu->body, 2 );
+
+        return json_decode ( '[' . $menu[ 1 ] );
+    }
+
+    public function getAllMenus () {
+        $menus = Menu::all ();
+        for ($i = 0, $jsonMenus = [ ]; $i < count ( $menus ); $i++)
+        {
+            $strMenu                   = explode ( '[', $menus [ $i ]->body, 2 );
+            $jsonMenus[ $i ][ 'menu' ] = json_decode ( '[' . $strMenu[ 1 ] );
+            $jsonMenus[ $i ][ 'id' ]   = $menus[ $i ]->id;
+        }
+
+        return $jsonMenus;
+    }
 
 }
--- app/views/menus/admin/index.blade.php
+++ app/views/menus/admin/index.blade.php
@@ -7,7 +7,6 @@
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                    <th>#</th>
                     <th>Menu</th>
                     <th>Action</th>
                 </tr>
@@ -15,15 +14,25 @@
             <tbody>
                 @foreach($menus as $menu)
                     <tr>
-                        <td>{{ $menu->id }}</td>
-                        <td><pre>{{ $menu->body }}</pre></td>
+                        <td>{{ $menu[ 'id' ] }}</td>
                         <td>
-                            <a href="{{ route('admin.menus.edit', $menu->id) }}" title="Éditer le menu">
+                            <dl>
+                                @foreach($menu[ 'menu' ] as $menuName)
+                                    <dt>{{ $menuName->name }}</dt>
+                                    @foreach($menuName->content as $menuContent)
+                                        <dd>{{ $menuContent }}</dd>
+                                    @endforeach
+                                @endforeach
+                            </dl>
+                        </td>
+{{--                        <td><pre>{{ print_r($menu) }}</pre></td>--}}
+                        <td>
+                            <a href="{{ route('admin.menus.edit', $menu[ 'id' ]) }}" title="Éditer le menu">
                                 <button class="btn btn-primary">
                                     <span class="glyphicon glyphicon-pencil"></span>
                                 </button>
                             </a>
-                            <a href="{{ route('admin.menus.destroy', $menu->id) }}" title="Supprimer le menu">
+                            <a href="{{ route('admin.menus.destroy', $menu[ 'id' ]) }}" title="Supprimer le menu">
                                 <button class="btn btn-danger">
                                     <span class="glyphicon glyphicon-remove"></span>
                                 </button>
