dabb8c024cc4834b67512ad7f8c6bfa19587bca4
Quelu <quelumatagne@gmail.com>
2015-01-28 02:05:51 +0100

    Form inline block comments

--- app/views/rallyes/index.blade.php
+++ app/views/rallyes/index.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
     @if ($nextRallye)
-        <div id="slideMap" class=".parallax__map"></div>
+        <div id="slideMap" class="parallax__map"></div>
         <section class="parallax__body">
             <div itemscope itemtype="http://schema.org/FoodEvent">
                 <h2 itemprop="headline" class="gamma">Rendez-vous le<span itemprop="startDate" class="span--spacing">{{ date('d/m/Y' , strtotime($nextRallye->date)) }}</span>à<span itemprop="location" class="span--spacing">{{ $nextRallye->city }}</span></h2>
                 <!-- TODO Microdata sur la p itemprop="description" -->
                 {{ $nextRallye->body }}
+                <p>Retrouvez nous à &laquo;<span class="span--spacing hightlight">{{ $nextRallye->adress . ', ' . $nextRallye->postal_code . ' ' . $nextRallye->city }}</span>&raquo;</p>
             </div>
         </section>
         <div class="parallax__img parallax__img--six"></div>
@@ -15,19 +16,18 @@
             <div itemscope itemtype="http://schema.org/Thing">
                 <h2 itemprop="headline" class="gamma">Inscrivez-vous pour ce Resto-Rallye</h2>
                 <p itemprop="description">Désireux de nous rejoindre à notre prochain événement&nbsp;? Rien de plus simple, il suffit de remplir le formulaire ci-dessous. Dépèchez-vous&nbsp;! Il ne reste plus que<span class="span--spacing hightlight">30 places</span>disponible.</p>
-                {{ Form::open(['route' => 'sendMail', 'class' => 'form']) }}
-                    {{-- {{ Form::inputContact('participantsRallye', 'Nombre de participants', [ 'placeholder' => '1', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('nameRallye', 'Nom', [ 'placeholder' => 'Doe', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('surnameRallye', 'Prénom', [ 'placeholder' => 'John', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('adressRallye', 'Adresse', [ 'placeholder' => 'Rue de la Province, n° 10', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('emailRallye', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('cityRallye', 'Ville', [ 'placeholder' => 'Liège', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('pcRallye', 'Code Postal', [ 'placeholder' => '4400', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('telRallye', 'Tel ou GSM', [ 'placeholder' => '0498 32 72 89', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('faxRallye', 'Fax', [ 'placeholder' => '063 14 85 62', 'class' => 'input__text' ], $errors) }} --}}
-                    {{-- {{ Form::inputContact('mailRallye', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('confirmMailRallye', 'Confirmation de mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{ Form::textareaContact('contentRallye', 'Votre message', [ 'placeholder' => 'Quelque chose à rajouter', 'class' => 'input__text' ], $errors) }}
+                {{ Form::open(['route' => 'sendMail', 'class' => 'form']) }}{{--
+                     --}}{{ Form::inputContact('participantsRallye', 'Nombre de participants', [ 'placeholder' => '1', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('nameRallye', 'Nom', [ 'placeholder' => 'Doe', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('surnameRallye', 'Prénom', [ 'placeholder' => 'John', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('adressRallye', 'Adresse', [ 'placeholder' => 'Rue de la Province, n° 10', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('cityRallye', 'Ville', [ 'placeholder' => 'Liège', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('pcRallye', 'Code Postal', [ 'placeholder' => '4400', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('telRallye', 'Tel ou GSM', [ 'placeholder' => '0498 32 72 89', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('faxRallye', 'Fax', [ 'placeholder' => '063 14 85 62', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('mailRallye', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('confirmMailRallye', 'Confirmation de mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} {{--
+                    --}}{{ Form::textareaContact('contentRallye', 'Votre message', [ 'placeholder' => 'Quelque chose à rajouter', 'class' => 'input__text' ], $errors) }}
                     <p class="hightlight label__text">Tous les champs munis d’un astérisque<span class="span--spacing asterisque">(*)</span>sont obligatoires</p>
                     {{ Form::submitContact('Envoyez') }}
                 {{ Form::close() }}
@@ -38,19 +38,18 @@
             <div itemscope itemtype="http://schema.org/Thing">
                 <h2 itemprop="headline" class="gamma">Invitez vos amis à ce Resto-Rallye</h2>
                 <p itemprop="description">Vous pouvez aussi décider d'offrir des places pour le prochain événement à l'un de vos amis. Vous n'avez qu'à remplir ce formulaire, nous nous occuperons de lui envoyer l'invitation. Dépèchez-vous&nbsp;! Il ne reste plus que<span class="span--spacing hightlight">30 places</span>disponible.</p>
-                {{ Form::open(['route' => 'sendMail', 'class' => 'form']) }}
-                    {{-- {{ Form::inputContact('participantsInvitation', 'Nombre de participants', [ 'placeholder' => '1', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('nameInvitation', 'Nom', [ 'placeholder' => 'Doe', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('surnameInvitation', 'Prénom', [ 'placeholder' => 'John', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('adressInvitation', 'Adresse', [ 'placeholder' => 'Rue de la Province, n° 10', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('emailInvitation', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('cityInvitation', 'Ville', [ 'placeholder' => 'Liège', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('pcInvitation', 'Code Postal', [ 'placeholder' => '4400', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('telInvitation', 'Tel ou GSM', [ 'placeholder' => '0498 32 72 89', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('faxInvitation', 'Fax', [ 'placeholder' => '063 14 85 62', 'class' => 'input__text' ], $errors) }} --}}
-                    {{-- {{ Form::inputContact('mailInvitation', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{-- {{ Form::inputContact('confirmMailInvitation', 'Confirmation de mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }} --}}
-                    {{ Form::textareaContact('contentInvitation', 'Votre message', [ 'placeholder' => 'Quelque chose à rajouter', 'class' => 'input__text' ], $errors) }}
+                {{ Form::open(['route' => 'sendMail', 'class' => 'form']) }}{{--
+                     --}}{{ Form::inputContact('participantsInvitation', 'Nombre de participants', [ 'placeholder' => '1', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('nameInvitation', 'Nom', [ 'placeholder' => 'Doe', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('surnameInvitation', 'Prénom', [ 'placeholder' => 'John', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('adressInvitation', 'Adresse', [ 'placeholder' => 'Rue de la Province, n° 10', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('cityInvitation', 'Ville', [ 'placeholder' => 'Liège', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('pcInvitation', 'Code Postal', [ 'placeholder' => '4400', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('telInvitation', 'Tel ou GSM', [ 'placeholder' => '0498 32 72 89', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('faxInvitation', 'Fax', [ 'placeholder' => '063 14 85 62', 'class' => 'input__text' ], $errors) }}{{--
+                     --}}{{ Form::inputContact('mailInvitation', 'Mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }}{{--
+                     --}}{{ Form::inputContact('confirmMailInvitation', 'Confirmation de mail', [ 'placeholder' => 'johndoe@gmail.com', 'class' => 'input__text' ], $errors, true) }}{{--
+                    --}}{{ Form::textareaContact('contentInvitation', 'Votre message', [ 'placeholder' => 'Quelque chose à rajouter', 'class' => 'input__text' ], $errors) }}
                     <p class="hightlight label__text">Tous les champs munis d’un astérisque<span class="span--spacing asterisque">(*)</span>sont obligatoires</p>
                     {{ Form::submitContact('Envoyez') }}
                 {{ Form::close() }}
